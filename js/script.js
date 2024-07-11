9ba6581ab7fb0d4f19e0d7a701f7ece6f521521e
fede9087 <fede9087@gmail.com>
2024-07-10 21:36:57 -0300

    Agrego todos los productos en products.html y ultimos ingregos en index

--- js/script.js
+++ js/script.js
@@ -30,3 +30,29 @@ setInterval(() => {
 }, 3000); // Cambiar la imagen cada 3 segundos
 
 
+// Obtener los últimos 10 productos  y actualizar la sección de productos
+fetch('https://comerciando.pythonanywhere.com/productos')
+    .then(response => response.json())
+    .then(data => {
+      const productContainer = document.querySelector('.products-container');
+      data.slice(-10).forEach(product => {
+        const productCard = document.createElement('div');
+        productCard.classList.add('product-card');
+        productCard.innerHTML = `
+          <a href="./detail.html?id=${product.id_producto}">
+            <div class="product-img">
+              <img src="${product.imagen}" alt="${product.nombre}">
+              <span class="promo">Nuevo ingreso</span>
+            </div>
+          </a>
+          <div class="product-info">
+            <h3>${product.nombre}</h3>
+            <strong>$ ${product.precio}</strong>
+            <span class="rating">★★★★☆</span>
+            <a href="#" class="add-cart">Añadir al carrito</a>
+          </div>
+        `;
+        productContainer.appendChild(productCard);
+      });
+    })
+    .catch(error => console.error('Error fetching products:', error));
